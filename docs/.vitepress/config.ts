ed385c62a6c0415422685c75c3c75956eb10bbf8
xxhls <xxhls0208@gmail.com>
2024-09-28 04:35:27 +0800

    build: 修改docs打包配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/vite-library-ts/',
   title: "My Awesome Project",
   description: "A VitePress Site",
   themeConfig: {
